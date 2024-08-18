2cffb6af0cedcef477aceee3dbb50c88589a0ad4
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-08-18 19:23:04 +0300

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.zip.InflaterInputStream;
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+   // System.out.println("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
 
