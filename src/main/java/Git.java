d321201698532b79d0aaecf6e89bf2e734bccbf8
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-08-22 17:17:25 +0300

    try something

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -297,7 +297,7 @@ public class Git {
             // Construiește un șir cu numele sortate
             StringBuilder sortedNames = new StringBuilder();
             for (String name : nameResult) {
-                sortedNames.append(name).append('\n');
+                sortedNames.append(name);
             }
             return sortedNames.toString().getBytes(StandardCharsets.ISO_8859_1);
         }
