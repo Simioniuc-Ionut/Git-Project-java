4e6db3c4b4f1c0995d81d14efcd2158d3ae26125
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-08-23 18:26:25 +0300

    try something

--- src/main/java/Commit.java
+++ src/main/java/Commit.java
@@ -1,20 +1,83 @@
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-@AllArgsConstructor
+
 public class Commit {
-    @Getter @Setter private String author;
-    @Getter @Setter private String committer;
-    @Getter @Setter private String tree_hash;
-    @Getter @Setter private String parent_commit_hash;
-    @Getter @Setter private LocalDateTime timestamp;
-    @Getter @Setter private String message;
+    private String author;
+    private String committer;
+    private String tree_hash;
+    private String parent_commit_hash;
+    private LocalDateTime timestamp;
+    private String message;
+
+    // Constructor
+    public Commit(String author, String committer, String tree_hash, String parent_commit_hash, LocalDateTime timestamp, String message) {
+        this.author = author;
+        this.committer = committer;
+        this.tree_hash = tree_hash;
+        this.parent_commit_hash = parent_commit_hash;
+        this.timestamp = timestamp;
+        this.message = message;
+    }
+
+    // Getters and Setters
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCommitter() {
+        return committer;
+    }
+
+    public void setCommitter(String committer) {
+        this.committer = committer;
+    }
+
+    public String getTreeHash() {
+        return tree_hash;
+    }
+
+    public void setTreeHash(String tree_hash) {
+        this.tree_hash = tree_hash;
+    }
+
+    public String getParentCommitHash() {
+        return parent_commit_hash;
+    }
+
+    public void setParentCommitHash(String parent_commit_hash) {
+        this.parent_commit_hash = parent_commit_hash;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     @Override
     public String toString() {
-        return String.format(
-                "tree %s\nparent %s\nauthor %s %s\ncommitter %s %s\n\n%s\n", tree_hash,
-                parent_commit_hash, author, timestamp.toString(), author,
-                timestamp.toString(), message);
+        // Adjusting format to match desired output
+        StringBuilder sb = new StringBuilder();
+        sb.append("tree ").append(tree_hash).append("\n");
+        if (parent_commit_hash != null && !parent_commit_hash.isEmpty()) {
+            sb.append("parent ").append(parent_commit_hash).append("\n");
+        }
+        sb.append("author ").append(author).append(" ").append(timestamp.toString()).append("\n");
+        sb.append("committer ").append(committer).append(" ").append(timestamp.toString()).append("\n");
+        sb.append("\n").append(message).append("\n");
+        return sb.toString();
     }
 }
